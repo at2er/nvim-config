13b1ca4d4c7474e5a196938a5f2b2a8b899087d9
at2er <xb0515@outlook.com>
2024-12-27 17:14:47 +0800

    feat: fast write to file and quit.
    (And some indent adjument.)

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -4,28 +4,32 @@ local opt = { noremap = true, silent = true }
 -- Don't put any plugin's mappings in this table.
 M.mappings =  {}
 M.mappings.n = {
-    ['<c-n>'] = "<cmd>bnext<cr>",
-    ['<c-p>'] = "<cmd>bprev<cr>",
-    ['<c-h>'] = "<c-w>h",
-    ['<c-j>'] = "<c-w>j",
-    ['<c-k>'] = "<c-w>k",
-    ['<c-l>'] = "<c-w>l",
-    ['<leader>nh'] = "<cmd>set nohlsearch<cr>",
-    ['<leader>ll'] = "<cmd>Lazy<cr>",
+  ['<c-n>'] = "<cmd>bnext<cr>",
+  ['<c-p>'] = "<cmd>bprev<cr>",
+  ['<c-h>'] = "<c-w>h",
+  ['<c-j>'] = "<c-w>j",
+  ['<c-k>'] = "<c-w>k",
+  ['<c-l>'] = "<c-w>l",
+  ['<leader>nh'] = "<cmd>set nohlsearch<cr>",
+  ['<leader>ll'] = "<cmd>Lazy<cr>",
+  ['<leader>w'] = "<cmd>w<cr>",
+  ['<leader>q'] = "<cmd>q<cr>",
+  ['<leader>b'] = "<cmd>b#<cr>",
+  ['<leader>s'] = "<cmd>buffers<cr>:",
 }
 
 M.mappings.v = {
-    ['<c-f>'] = '<right>',
-    ['<c-b>'] = '<left>',
-    ['<c-n>'] = '<down>',
-    ['<c-p>'] = '<up>',
+  ['<c-f>'] = '<right>',
+  ['<c-b>'] = '<left>',
+  ['<c-n>'] = '<down>',
+  ['<c-p>'] = '<up>',
 }
 
 M.mappings.i = {
-    ['<c-f>'] = '<right>',
-    ['<c-b>'] = '<left>',
-    ['<c-n>'] = '<down>',
-    ['<c-p>'] = '<up>',
+  ['<c-f>'] = '<right>',
+  ['<c-b>'] = '<left>',
+  ['<c-n>'] = '<down>',
+  ['<c-p>'] = '<up>',
 }
 
 -- Put plugin's mappings in this table only.
@@ -34,68 +38,60 @@ M.plugin = {}
 M.plugin.auto_set = { "telescope", "conform", "neotree" }
 
 M.plugin.telescope = function ()
-    return {
-        n = {
-            ['<leader>ff'] = "<cmd>Telescope find_files<cr>",
-            ['<leader>fg'] = "<cmd>Telescope live_grep<cr>",
-            ['<leader>fb'] = "<cmd>Telescope buffers<cr>",
-            ['<leader>fh'] = "<cmd>Telescope help_tags<cr>",
-        }
+  return {
+    n = {
+      ['<leader>ff'] = "<cmd>Telescope find_files<cr>",
+      ['<leader>fg'] = "<cmd>Telescope live_grep<cr>",
+      ['<leader>fb'] = "<cmd>Telescope buffers<cr>",
+      ['<leader>fh'] = "<cmd>Telescope help_tags<cr>",
     }
+  }
 end
 
 M.plugin.cmp = function (cmp)
-    local mapping = cmp.mapping
-    return {
-        ['<c-b>'] = mapping.scroll_docs(-4),
-        ['<c-f>'] = mapping.scroll_docs(4),
-        ['<c-n>'] = mapping.select_next_item(),
-        ['<c-p>'] = mapping.select_prev_item(),
-        ['<c-o>'] = mapping.complete(),
-        ['<c-e>'] = mapping.abort(),
-        ['<tab>'] = mapping.confirm({ select = true }),
-    }
+  local mapping = cmp.mapping
+  return {
+    ['<c-b>'] = mapping.scroll_docs(-4),
+    ['<c-f>'] = mapping.scroll_docs(4),
+    ['<c-n>'] = mapping.select_next_item(),
+    ['<c-p>'] = mapping.select_prev_item(),
+    ['<c-o>'] = mapping.complete(),
+    ['<c-e>'] = mapping.abort(),
+    ['<tab>'] = mapping.confirm({ select = true }),
+  }
 end
 
 M.plugin.conform = function ()
-    return {
-        n = {
-            ['<leader>F'] = function ()
-                local conform = require('conform')
-                conform.format({ async = true })
-            end
-        }
+  return {
+    n = {
+      ['<leader>F'] = function ()
+        local conform = require('conform')
+        conform.format({ async = true })
+      end
     }
+  }
 end
 
 M.plugin.lsp = function ()
-    local lsp = vim.lsp
-    return {
-        n = {
-            ['gh'] = lsp.buf.hover,
-            ['gll'] = vim.diagnostic.open_float,
-            ['gd'] = lsp.buf.definition,
-            ['gD'] = lsp.buf.declaration,
-            ['gr'] = lsp.buf.references,
-        }
+  local lsp = vim.lsp
+  return {
+    n = {
+      ['gh'] = lsp.buf.hover,
+      ['gll'] = vim.diagnostic.open_float,
+      ['gd'] = lsp.buf.definition,
+      ['gD'] = lsp.buf.declaration,
+      ['gr'] = lsp.buf.references,
+      ['ga'] = lsp.buf.code_action,
     }
+  }
 end
 
---M.plugin.ufo = function (ufo)
---    return {
---        n = {
---            ['zR'] = ufo.openAllFolds,
---            ['zM'] = ufo.closeAllFolds,
---        }
---    }
---end
-
 M.plugin.neotree = function ()
-    return {
-        n = {
-            ['<leader>e'] = "<cmd>Neotree toggle<cr>"
-        }
+  return {
+    n = {
+      ['<leader>e'] = "<cmd>Neotree toggle<cr>"
     }
+  }
 end
 
 return M
--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -3,55 +3,58 @@ local M = {}
 vim.scriptencoding = 'utf-8'
 
 M.config = {
-    noplugin = false,
-    --default = {
-    --    keymap_option = { noremap = true, silent = true },
-    --},
+  noplugin = false,
+  colorscheme = "simple-night",
+  --default = {
+  --    keymap_option = { noremap = true, silent = true },
+  --},
 }
 
 M.options = {
-    clipboard = 'unnamedplus',
-    encoding = 'utf-8',
-    fileencoding = 'utf-8',
+  clipboard = 'unnamedplus',
+  encoding = 'utf-8',
+  fileencoding = 'utf-8',
 
-    relativenumber = true,
-    number = true,
+  relativenumber = true,
+  number = true,
 
-    title = true,
-    autoindent = true,
-    hlsearch = true,
-    backup = false,
-    backupskip = '/tmp/*,',
-    showcmd = true,
-    showmode = false,
-    cmdheight = 1,
-    laststatus = 2,
-    expandtab = true,
-    scrolloff = 5,
-    inccommand = 'split',
-    ignorecase = true,
-    smarttab = true,
-    breakindent = true,
-    shiftwidth = 4,
-    tabstop = 4,
-    colorcolumn = "80",
-    ai = true,
-    si = true,
-    wrap = false,
-    backspace = 'start,eol,indent',
+  title = true,
+  autoindent = true,
+  hlsearch = true,
+  backup = false,
+  backupskip = '/tmp/*,',
+  showcmd = true,
+  showmode = false,
+  cmdheight = 1,
+  laststatus = 2,
+  expandtab = false,
+  scrolloff = 5,
+  inccommand = 'split',
+  ignorecase = true,
+  smarttab = true,
+  breakindent = true,
+  shiftwidth = 8,
+  tabstop = 8,
+  colorcolumn = "80",
+  ai = true,
+  si = true,
+  wrap = true,
+  backspace = 'start,eol,indent',
 
-    cursorline = true,
-    termguicolors = true,
-    background = 'dark',
+  cursorline = false,
+  termguicolors = true,
+  background = 'dark',
 
-    signcolumn = 'yes',
+  signcolumn = 'yes',
 
-    fillchars = [[eob: ]],
+  fillchars = [[eob: ]],
+  list = true,
+  listchars = [[tab:>-,trail:+]],
 }
 
 M.globals = {
-    maplocalleader = ' ',
-    mapleader = ' ',
+  maplocalleader = ' ',
+  mapleader = ' ',
 }
 
 return M
